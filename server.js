1f9dea49f4a396cb91812c98026f3c0ba4f6ad79
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2023-10-27 01:02:47 +0100

    Update Server to Upgrade HTTP Connections to HTTPS

--- server.js
+++ server.js
@@ -1,28 +1,53 @@
 const express = require('express')
-const http = require('http')
+const fs = require('fs')
 const clc = require('cli-color')
+const argv = require('yargs')(process.argv.slice(2)).command('dev', 'Run the server in development mode').alias('dev', 'd').argv
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
+app.enable('trust proxy')
+app.use(express.urlencoded({ extended: true }))
 
 app.use(LogConnections)
 
+app.all('*', (req, res, next) => {
+  var isLocal = req.socket.localAddress === req.socket.remoteAddress
+  if (!argv.dev && !req.secure && !isLocal) {
+    res.redirect(308, 'https://' + req.headers.host + req.url)
+	console.log(`${logTimestamp} ${clc.bgRed.white('308')} Upgrading connection`)
+  } else next()
+})
+
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get(['/', '/index', '/index.(html|php)'], (req, res) => {
   res.render('index')
 })
+
 
 app.all('*', (req, res) => {
   res.sendStatus(404)
 })
 
 function LogConnections(req, res, next) {
-  var date = new Date()
-  var logTimestamp =
+  console.log(
+    `${logTimestamp} ${clc.inverse(req.method)} request for ${clc.underline(req.url)} from ${clc.cyan(
+      req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress
+    )}`
+  )
+  next()
+}
+
+app.listen(port, () => {
+  if (argv.dev) console.log(`${clc.yellow(`Server in development mode ${clc.bold('NO SSL')}`)}`)
+  console.log(`${clc.green(`Listening on port ${port}`)}`)
+})
+
+var date = new Date(),
+  logTimestamp =
     ('00' + (date.getMonth() + 1)).slice(-2) +
     '/' +
     ('00' + date.getDate()).slice(-2) +
@@ -34,15 +59,3 @@ function LogConnections(req, res, next) {
     ('00' + date.getMinutes()).slice(-2) +
     ':' +
     ('00' + date.getSeconds()).slice(-2)
-
-  console.log(
-    `${logTimestamp} ${clc.inverse(req.method)} request for ${clc.underline(req.url)} from ${clc.cyan(
-      req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress
-    )}`
-  )
-  next()
-}
-
-app.listen(port, () => {
-  console.log(`${clc.green(`Listening on port ${port}`)}`)
-})
