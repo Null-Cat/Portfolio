3adb2b059ed34490dd50d0c9ab8e59f2cef75b95
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2023-11-08 17:11:15 +0000

    Add PM2 config for Deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  apps: [
+    {
+      // Name of app
+      name: 'PhilipPortfolio',
+      // Script for pm2 run forever
+      // If use static website, remove it
+      script: 'server.js',
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+
+      // Args for script for pm2 run forever
+      // If use static website, remove it
+      //args: 'one two',
+      // Current directory on server
+      cwd: '/var/www/production/current',
+      // Config out file for web errors
+      error_file: '/var/www/production/logs/web.err.log',
+      // Config out file for web logs
+      out_file: '/var/www/production/logs/web.out.log',
+      // Number of instances to be started in cluster mode
+      instances: 1,
+      // Enable or disable auto restart after process failure
+      autorestart: true,
+      // Enable or disable the watch mode
+      watch: false,
+      // Restart the app if an amount of memory is exceeded (format: /0-9?/ K for KB, ‘M’ for MB, ‘G’ for GB, default to B)
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      // ^env_\S*$ => Specify environment variables to be injected when using –env
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      // SSH user
+      user: 'phro',
+      // SSH host
+      host: 'nullcat.tech',
+      // GIT remote/branch
+      ref: 'origin/main',
+      // GIT remote
+      repo: 'git@github.com:Null-Cat/Portfolio.git',
+      // Fetch all branches or fast
+      fetch: 'all',
+      // Path in the server
+      path: '/var/www/production',
+      'post-setup': "ls -la",
+      // Command run after pull source code
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
