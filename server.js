b298b52e5a36888f45d460f7c0dea008f21a5227
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2024-08-10 07:45:01 +0100

    Fix Server to Obtain True IP from Cloudflare Proxy

--- server.js
+++ server.js
@@ -289,13 +289,8 @@ function getServerIP() {
  * @returns {string} The true IP address.
  */
 function getTrueIP(req) {
-  console.log(req.headers["x-forwarded-for"]);
-  console.log(req.headers["X-Forwarded-For"]);
-  console.log(req.headers["CF-Connecting-IP"]);
-  console.log(req.headers["cf-connecting-ip"]);
-
   return req.headers["x-forwarded-for"]
-    ? req.headers["x-forwarded-for"]
+    ? req.headers["x-forwarded-for"].split(",")[0]
     : req.socket.remoteAddress.replace("::ffff:", "");
 }
 
